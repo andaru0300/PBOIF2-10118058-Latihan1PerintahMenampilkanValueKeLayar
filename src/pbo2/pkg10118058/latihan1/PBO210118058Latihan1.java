d8445267ba4279b5a28708c52c55324eb62bd491
Satyaning Andaru Bawalaksana <55575075+andaru0300@users.noreply.github.com>
2019-09-24 17:45:16 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118058/latihan1/PBO210118058Latihan1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118058.latihan1;
+
+/**
+ *
+ * @author user
+ * Nama         :   Satyaning Andaru Bawalaksana
+ * Nim          :   10118058
+ * Kelas        :   PBOIF2
+ * Deskripsi    :   Program ini berisi program menampilkan
+ * Jenis-jenis tipe data bilangan bulat
+ */
+public class PBO210118058Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Tulis kode disini
+        System.out.println("Biodata Rockstar"+"\n"+"=============================================="+"\n");
+        System.out.println("Nama \t \t : Satyaning Andaru Bawalaksana");
+        System.out.println("Alamat \t \t : Bandung");
+        System.out.println("Tempat lahir  \t : Klaten");
+        System.out.println("Tanggal lahir  \t : 03 - 09 - 2000"+"\n");
+        System.out.println("==========================================="+"\n");
+        System.out.println("Hobi \t \t : Memainkan Alat musik");
+        System.out.println("Makes \t \t : Nasgor");
+        System.out.println("Mikes \t\t : Air putih");
+    }
+    
+}
